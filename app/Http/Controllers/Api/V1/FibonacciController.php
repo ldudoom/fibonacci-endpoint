3b61550c1300d22c4ce608ab1ce3f3a0986dccef
Raul Chauvin <raul.chauvin@telefonica.com>
2022-05-22 14:37:14 -0500

    Publicando metodo en el controlador

--- app/Http/Controllers/Api/V1/FibonacciController.php
+++ app/Http/Controllers/Api/V1/FibonacciController.php
@@ -4,20 +4,23 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use App\Interfaces\FibonacciInterface;
 
 class FibonacciController extends Controller
 {
+
+    protected FibonacciInterface $_fibonacci;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(FibonacciInterface $fibonacci)
     {
-        //
+        $this->_fibonacci = $fibonacci;
     }
 
     public function getFibonacciValue(int $index): JsonResponse{
-        return response()->json([$index], Response::HTTP_OK);
+        return response()->json(['fibonacci_value' => $this->_fibonacci->getValue($index)], Response::HTTP_OK);
     }
 }
